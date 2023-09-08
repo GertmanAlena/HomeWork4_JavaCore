5f19042840b209e52be8a3d5509027c9a19704d0
Alena <gertmanea@mail.ru>
2023-09-08 17:13:31 +0300

    "4 ДЗ  второй вариант работы"

--- src/main/java/HomeWork4/Main.java
+++ src/main/java/HomeWork4/Main.java
@@ -1,41 +1,56 @@
 package HomeWork4;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int sum = 0;
-        // region Задача1
-        String[][] mass = {
-            {"22", "4", "6", "0"},
-            {"1", "31", "5", "5"},
-            {"8", "3", "42", "10"},
-            {"85", "3", "5", "69"},
-        };
-        String[][] mass2 = {
-                {"22", "4", "6"},
-                {"1", "31", "5"},
-                {"8", "3", "42"},
-                {"85"},
-        };
+        Random random = new Random();
+//        int sum = 0;
+//        // region Задача1
+//
+//
+//        String[][] mass = {
+//            {"22", "4", "6", "0"},
+//            {"1", "31", "5", "5"},
+//            {"8", "3", "42", "10"},
+//            {"85", "3", "5", "69"},
+//        };
+//        String[][] mass2 = {
+//                {"22", "4", "6"},
+//                {"1", "31", "5"},
+//                {"8", "3", "42"},
+//                {"85"},
+//        };
+//
+//        try{
+//            NewArray.add(mass);
+//            NewArray.add(mass2);
+//        }
+//        catch (ExceptionAll e){
+//            System.out.println("Массив не обработан ->" + e.getMessage());
+//        }
+//
+//        String[][] mass3 = {
+//                {"22", "-", "6", "0"},
+//                {"1", "31", "5", "5"},
+//                {"8", "*", "42", "10"},
+//                {"85", "3", "5", "69"},
+//        };
+//        try{
+//            sum = NewArray.summa(mass3);
+//            System.out.println(sum);
+//        } catch (ExceptionAll e){
+//            System.out.println(e.getMessage());
+//        }
+//        //endregion
 
+        //region Второй вариант решения задания
         try{
-            NewArray.add(mass);
-            NewArray.add(mass2);
-        }
-        catch (ExceptionAll e){
-            System.out.println("Массив не обработан ->" + e.getMessage());
-        }
-        //endregion
-        String[][] mass3 = {
-                {"22", "-", "6", "0"},
-                {"1", "31", "5", "5"},
-                {"8", "*", "42", "10"},
-                {"85", "3", "5", "69"},
-        };
-        try{
-            sum = NewArray.summa(mass3);
-            System.out.println(sum);
+            NewArray.generatedMass(random.nextInt(3,5), random.nextInt(3,5));
         } catch (ExceptionAll e){
             System.out.println(e.getMessage());
         }
+        //endregion
+
     }
 }
--- src/main/java/HomeWork4/NewArray.java
+++ src/main/java/HomeWork4/NewArray.java
@@ -1,49 +1,88 @@
 package HomeWork4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class NewArray {
 
-    /**
-     *
-     * @param mass
-     * @return
-     * @throws MyArraySizeException исключение некорректного размера массива
-     */
-    public static Integer add(String[][] mass) throws MyArraySizeException{
-        if (mass.length != 4)
-            throw new MyArraySizeException();
-        for (int i = 0; i < mass.length; i++) {
-            System.out.println();
-            if (mass[i].length != 4)
-                throw new MyArraySizeException();
-            for (int j = 0; j < mass[i].length; j++) {
-                System.out.print(mass[i][j] + " ");
-            }
-        }
-        return null;
-    }
+    static Random random = new Random();
+    static Scanner scanner = new Scanner(System.in);
+//    /**
+//     *
+
+//     * @param mass
+//     * @return
+//     * @throws MyArraySizeException исключение некорректного размера массива
+//     */
+//    public static Integer print(String[][] mass) throws MyArraySizeException{
+//        if (mass.length != 4)
+//            throw new MyArraySizeException();
+//        for (int i = 0; i < mass.length; i++) {
+//            System.out.println();
+//            if (mass[i].length != 4)
+//                throw new MyArraySizeException();
+//            for (int j = 0; j < mass[i].length; j++) {
+//                System.out.print(mass[i][j] + " ");
+//            }
+//        }
+//        return null;
+//    }
+
+//    /**
+//     *
+//     * @param mass
+//     * @return
+//     * @throws MyArraySizeException
+//     * @throws MyArrayDataException
+//     */
+//    public static Integer summa(String[][] mass) throws MyArraySizeException, MyArrayDataException{
+//        int sum = 0;
+//        if (mass.length != 4)
+//            throw new MyArraySizeException();
+//        for (int i = 0; i < mass.length; i++) {
+//            if (mass[i].length != 4) throw new MyArraySizeException();
+//            for (int j = 0; j < mass[i].length; j++) {
+//                try{
+//                    sum += Integer.parseInt(mass[i][j]);
+//                } catch (NumberFormatException e){
+//                    throw new MyArrayDataException(i, j);
+//                }
+//            }
+//        }
+//        return sum;
+//    }
 
     /**
-     *
-     * @param mass
-     * @return
-     * @throws MyArraySizeException
-     * @throws MyArrayDataException
+     * Второй вариант работы, всё в одном методе. Размер массива рандомно
+     * заполнение массива с консоли
+     * @param i количество строк
+     * @param j количество столбцов
+     * @throws MyArraySizeException исключение некорректного размера массива
+     * @throws MyArrayDataException исключение некорректного символа массива для суммирования
      */
-    public static Integer summa(String[][] mass) throws MyArraySizeException, MyArrayDataException{
+    public static void generatedMass(int i, int j) throws MyArraySizeException, MyArrayDataException{
         int sum = 0;
-        if (mass.length != 4)
-            throw new MyArraySizeException();
-        for (int i = 0; i < mass.length; i++) {
-            if (mass[i].length != 4) throw new RuntimeException();
-            for (int j = 0; j < mass[i].length; j++) {
+        if(i != 4 || j != 4) throw new MyArraySizeException();
+        System.out.println(i);
+        System.out.println(j);
+        String array[][] = new String[i][j];
+        for (int k = 0; k < i; k++) {
+            for (int l = 0; l < j; l++) {
+                System.out.println("Введите символ");
+                array[k][l] = scanner.nextLine();
                 try{
-                    sum += Integer.parseInt(mass[i][j]);
+                    sum += Integer.parseInt(array[k][l]);
                 } catch (NumberFormatException e){
-                    throw new MyArrayDataException(i, j);
+                    throw new MyArrayDataException(k, l);
                 }
-            }
+            }System.out.println();
+            System.out.println(sum);
         }
-        return sum;
+        for (int z = 0; z < array.length; z++) {
+            for (int x = 0; x < array[z].length; x++) {
+                System.out.print(array[z][x] + " ");
+            }System.out.println();
+        }
     }
 
 }
